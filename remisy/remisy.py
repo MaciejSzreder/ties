f90822fe8ed3e1c6fc37641d1e01e77ef28d9d64
Maciej Szreder <MSzreder3.14@wp.pl>
2021-07-07 23:17:33 +0200

    section arguments parsing off

--- remisy/remisy.py
+++ remisy/remisy.py
@@ -13,51 +13,11 @@
 import xml.etree.ElementTree as ET
 import re
 import sys
-import argparse
 
-import default
 
 from  HTMLtoXML import HTML2XML
 
-#get command line arguments
-argparser=argparse.ArgumentParser(description="This program load data of match results form source file (in html syntax) and create file (csv) with distance between ties")
-argparser.add_argument(
-    "--source","--src","-in",
-    dest="source",
-    default=default.PATH_DATA,
-    help="Path to file with data. File have to be html structure."
-)
-argparser.add_argument(
-    "--result","--rzt","-out",
-    dest="result",
-    default=default.PATH_RESOULT,
-    help="Path to file with data. File have to be html structure."
-)
-argparser.add_argument(
-    "--path1","-p1",
-    dest="path1",
-    default=default.PATH_FIRST,
-    help="XPath to the element containing text with first data"
-)
-argparser.add_argument(
-    "--path2","-p2",
-    dest="path2",
-    default=default.PATH_SECOND,
-    help="XPath to the element containing text with second data"
-)
-argparser.add_argument(
-    "--regexp1","-r1",
-    dest="exp1",
-    default=default.EXPR_FIRST,
-    help="Pattern of first data in text."
-)
-argparser.add_argument(
-    "--regexp2","-r2",
-    dest="exp2",
-    default=default.EXPR_SECOND,
-    help="Pattern of second data in text."
-)
-args=argparser.parse_args()
+from arguments import args
 
 dataPath=args.source;
 outDataPath=args.result;
