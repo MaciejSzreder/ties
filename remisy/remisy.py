c59b8e7539fd6a3e4de37127d049262cb5f8634a
Maciej Szreder <MSzreder3.14@wp.pl>
2021-07-07 23:12:52 +0200

    section default values off

--- remisy/remisy.py
+++ remisy/remisy.py
@@ -8,18 +8,14 @@
 
 #-p1 ".//*[@class='fin']" -r1 "\d+" -p2 ".//*[@class='fin']" -r2 "(?<=:)\d+"
 
-PATH_DATA="data.htm"
-PATH_RESOULT="result.csv"
 
-PATH_FIRST=".//*[@class='event__scores fontBold']/span[1]"
-PATH_SECOND=".//*[@class='event__scores fontBold']/span[2]"
-EXPR_FIRST='\d+'
-EXPR_SECOND='\d+'
 
 import xml.etree.ElementTree as ET
 import re
 import sys
 import argparse
+
+import default
 
 from  HTMLtoXML import HTML2XML
 
@@ -28,37 +24,37 @@ argparser=argparse.ArgumentParser(description="This program load data of match r
 argparser.add_argument(
     "--source","--src","-in",
     dest="source",
-    default=PATH_DATA,
+    default=default.PATH_DATA,
     help="Path to file with data. File have to be html structure."
 )
 argparser.add_argument(
     "--result","--rzt","-out",
     dest="result",
-    default=PATH_RESOULT,
+    default=default.PATH_RESOULT,
     help="Path to file with data. File have to be html structure."
 )
 argparser.add_argument(
     "--path1","-p1",
     dest="path1",
-    default=PATH_FIRST,
+    default=default.PATH_FIRST,
     help="XPath to the element containing text with first data"
 )
 argparser.add_argument(
     "--path2","-p2",
     dest="path2",
-    default=PATH_SECOND,
+    default=default.PATH_SECOND,
     help="XPath to the element containing text with second data"
 )
 argparser.add_argument(
     "--regexp1","-r1",
     dest="exp1",
-    default=EXPR_FIRST,
+    default=default.EXPR_FIRST,
     help="Pattern of first data in text."
 )
 argparser.add_argument(
     "--regexp2","-r2",
     dest="exp2",
-    default=EXPR_SECOND,
+    default=default.EXPR_SECOND,
     help="Pattern of second data in text."
 )
 args=argparser.parse_args()
