e8bf8cbc750ae7335e7cfda193b33f903ad2ff2a
Maciej Szreder <MSzreder3.14@wp.pl>
2021-07-09 00:37:06 +0200

    corect arguments description

--- remisy/arguments.py
+++ remisy/arguments.py
@@ -3,42 +3,42 @@ import argparse
 import default
 
 #get command line arguments
-argparser=argparse.ArgumentParser(description="This program load data of match results form source file (in html syntax) and create file (csv) with distance between ties")
+argparser=argparse.ArgumentParser(description="This program load data of match results form source file (in html syntax) and create file (csv) with distance between ties.")
 argparser.add_argument(
     "--source","--src","-in",
     dest="source",
     default=default.PATH_DATA,
-    help="Path to file with data. File have to be html structure."
+    help="Path to file with data. File have to have html format."
 )
 argparser.add_argument(
     "--result","--rzt","-out",
     dest="result",
     default=default.PATH_RESOULT,
-    help="Path to file with data. File have to be html structure."
+    help="Path to file to append result. File will have csv format."
 )
 argparser.add_argument(
     "--path1","-p1",
     dest="path1",
     default=default.PATH_FIRST,
-    help="XPath to the element containing text with first data"
+    help="XPath to the element containing text with first team scores."
 )
 argparser.add_argument(
     "--path2","-p2",
     dest="path2",
     default=default.PATH_SECOND,
-    help="XPath to the element containing text with second data"
+    help="XPath to the element containing text with first team scores."
 )
 argparser.add_argument(
     "--regexp1","-r1",
     dest="exp1",
     default=default.EXPR_FIRST,
-    help="Pattern of first data in text."
+    help="Regular expression showing shape and position of first team scores."
 )
 argparser.add_argument(
     "--regexp2","-r2",
     dest="exp2",
     default=default.EXPR_SECOND,
-    help="Pattern of second data in text."
+    help="Regular expression showing shape and position of second team scores."
 )
 argparser.add_argument(
     "--direction","-d",
