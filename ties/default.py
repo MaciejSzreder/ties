7bfe5e431a54107336a5922854bc45bd9bbe9020
Maciej Szreder <MSzreder3.14@wp.pl>
2021-07-10 23:47:21 +0200

    Separe scripts

--- ties/default.py
+++ ties/default.py
@@ -1,3 +1,5 @@
+# If you want to change the default behaviour, duplicate line, cloned line ahead the '#' sign, modify value of the oryginal line.
+# Be careful this file is vulnreable for script injection, don't copy untrusted code nor value
 PATH_DATA="data.htm"
 PATH_RESOULT="result.csv"
 
