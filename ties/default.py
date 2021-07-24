2ea90ae840eca3035bc509c0d7a8b944b2f3670c
Maciej Szreder <MSzreder3.14@wp.pl>
2021-07-24 23:28:01 +0200

    New default parameters

--- ties/default.py
+++ ties/default.py
@@ -3,7 +3,7 @@
 PATH_DATA="data.htm"
 PATH_RESOULT="result.csv"
 
-PATH_FIRST=".//*[@class='event__scores fontBold']/span[1]"
-PATH_SECOND=".//*[@class='event__scores fontBold']/span[2]"
+PATH_FIRST=".//*[contains(@class,'first')]"
+PATH_SECOND=".//*[contains(@class,'second')]"
 EXPR_FIRST='\d+'
 EXPR_SECOND='\d+'
